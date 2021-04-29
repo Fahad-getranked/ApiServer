0adedc431efbfbdbed1efecc2a2610fb257040db
altitudemalaysia <dani@altitude.com.my>
2021-04-30 03:39:48 +0800

    Active Events

--- routes/mqtt_client.js
+++ routes/mqtt_client.js
@@ -180,65 +180,65 @@ syncdata.then(res=>{
 
 }
 function run_cron_for_gallagher_events(){
-// 	var intervaly = setInterval(function() 
-// 	{
-// 	var checkin_events;
-// 	var checkout_events;
-// 	var noentry_events;
-// 	var doors_events;
+	var intervaly = setInterval(function() 
+	{
+	var checkin_events;
+	var checkout_events;
+	var noentry_events;
+	var doors_events;
 	
-// 	var checkin=cron_mod.get_gallagher_all_events();
-// 	checkin.then(groups=>{
-// 		if(groups.length>0){
-// 			//console.log("Total Length="+groups.length);
-// 	 checkin_events=JSON.stringify(groups);
-// 	//console.log(checkin_events);
-// 	var syncdata=cron_mod.save_gg_checkin_checkout_events_in_server(checkin_events);
-// 	syncdata.then(res=>{
-// 	 //console.log(res);
-// 	});
-// 		}
-// 	});	
-// 	var dooeevent=cron_mod.get_gallagher_door_alarms();
-// 	dooeevent.then(groups=>{
-// 		if(groups.length>0){
-// 			doors_events=JSON.stringify(groups);
-// 	var syncdata=cron_mod.save_gg_ndoor_alarms_events_in_server(doors_events);
-// 	syncdata.then(res=>{
-// //console.log(res);
-// 	});
-// 		}
-// 	});
-// 	// var checkout=cron_mod.get_gallagher_checkout_events();
+	var checkin=cron_mod.get_gallagher_all_events();
+	checkin.then(groups=>{
+		if(groups.length>0){
+			//console.log("Total Length="+groups.length);
+	 checkin_events=JSON.stringify(groups);
+	//console.log(checkin_events);
+	var syncdata=cron_mod.save_gg_checkin_checkout_events_in_server(checkin_events);
+	syncdata.then(res=>{
+	 //console.log(res);
+	});
+		}
+	});	
+	var dooeevent=cron_mod.get_gallagher_door_alarms();
+	dooeevent.then(groups=>{
+		if(groups.length>0){
+			doors_events=JSON.stringify(groups);
+	var syncdata=cron_mod.save_gg_ndoor_alarms_events_in_server(doors_events);
+	syncdata.then(res=>{
+//console.log(res);
+	});
+		}
+	});
+	// var checkout=cron_mod.get_gallagher_checkout_events();
 	
-// 	// checkout.then(groups=>{
-// 	// 	if(groups.length>0){
-// 	// 		console.log("checkout Length"+groups.length);
-// 	// // checkout_events=JSON.stringify(groups);
-// 	// // var syncdata=cron_mod.save_gg_checkin_checkout_events_in_server(checkout_events);
-// 	// // syncdata.then(res=>{
-// 	// // //  console.log(res);
-// 	// // });
-// 	// 	}
+	// checkout.then(groups=>{
+	// 	if(groups.length>0){
+	// 		console.log("checkout Length"+groups.length);
+	// // checkout_events=JSON.stringify(groups);
+	// // var syncdata=cron_mod.save_gg_checkin_checkout_events_in_server(checkout_events);
+	// // syncdata.then(res=>{
+	// // //  console.log(res);
+	// // });
+	// 	}
+	// });
+	
+
+// 	var noentry=cron_mod.get_gallagher_no_entry_events();
+	
+// 	noentry.then(groups=>{
+// 		if(groups.length>0){
+// 	// 		noentry_events=JSON.stringify(groups);
+// 	// var syncdata=cron_mod.save_gg_noentry_events_in_server(noentry_events);
+// 	// syncdata.then(res=>{
+// 	//   //console.log(res);
 // 	// });
-	
-
-// // 	var noentry=cron_mod.get_gallagher_no_entry_events();
-	
-// // 	noentry.then(groups=>{
-// // 		if(groups.length>0){
-// // 	// 		noentry_events=JSON.stringify(groups);
-// // 	// var syncdata=cron_mod.save_gg_noentry_events_in_server(noentry_events);
-// // 	// syncdata.then(res=>{
-// // 	//   //console.log(res);
-// // 	// });
-// // 		}
-// // 	});
+// 		}
+// 	});
 
 
 	
 
-// 	  }, constants.DEFAULT_EVENT_CRON_JOB_TIME);
+	  }, constants.DEFAULT_EVENT_CRON_JOB_TIME);
 	
 }
 function run_cron_for_gallagher_delete_user(){
